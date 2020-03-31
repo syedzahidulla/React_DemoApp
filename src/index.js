ddaec69cbe7452e20dcb72455db0ec25d37a9ab2
syedzahidulla <szyaehdid@gmail.com>
2020-03-31 15:27:51 +0530

    First Commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(<div><h1>Hello World</h1><p>hi</p></div>,document.getElementById("root"))
+
+
+
+
